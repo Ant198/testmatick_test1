3fc41e77172a9fb337f97acb7d7d7849eb3b5f35
Anton <ridnovir@yahoo.com>
2025-02-07 13:31:22 +0200

    edit test

--- src/test/java/test_pages/BaseTest.java
+++ src/test/java/test_pages/BaseTest.java
@@ -7,11 +7,7 @@ import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
     private WebDriver driver;
-    private String baseUrl = "https://www.google.com.ua/";
-    private String currentUrl;
-    private String expectedText = "Java";
-    private String baseTitle;
-    private String currentTitle;
+
 
     public void setDriver(WebDriver driver) {
         this.driver = driver;
@@ -19,46 +15,6 @@ public class BaseTest {
 
     public WebDriver getDriver() {
         return driver;
-    }
-
-    public void setBaseUrl(String url) {
-        this.baseUrl = url;
-    }
-
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-
-    public void setCurrentUrl(String currentUrl) {
-        this.currentUrl = currentUrl;
-    }
-
-    public String getCurrentUrl() {
-        return currentUrl;
-    }
-
-    public void setExpectedText (String text){
-        this.expectedText = text;
-    }
-
-    public String getExpectedText () {
-        return expectedText;
-    }
-
-    public void setBaseTitle(String text){
-        this.baseTitle = text;
-    }
-
-    public String getBaseTitle () {
-        return baseTitle;
-    }
-
-    public void setCurrentTitle(String text){
-        this.currentTitle = text;
-    }
-
-    public String getCurrentTitle () {
-        return currentTitle;
     }
 
     @BeforeTest
--- src/test/java/test_pages/TestGoogleSearchResult.java
+++ src/test/java/test_pages/TestGoogleSearchResult.java
@@ -9,24 +9,29 @@ import pages.SearchPage;
 import java.time.Duration;
 
 public class TestGoogleSearchResult extends BaseTest {
+    String baseUrl = "https://www.google.com.ua/";
+    String currentUrl;
+    String expectedText = "Java";
+    String baseTitle;
+    String currentTitle;
 
     @Test
     public void isTestGoogleSearchResult() {
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         SearchPage searchPage = new SearchPage(getDriver());
-        setCurrentUrl(searchPage.getCurrentUrl());
-        setBaseTitle(searchPage.getPageTitle());
+        currentUrl = searchPage.getCurrentUrl();
+        baseTitle = searchPage.getPageTitle();
         searchPage.typeText();
         searchPage.clickButton();
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
         ResultPage resultPage = new ResultPage(getDriver());
-        setCurrentTitle(resultPage.getPageTitle());
+        currentTitle = resultPage.getPageTitle();
 
         for (WebElement header : resultPage.getListOfText()) {
             Assert.assertTrue(header.getText().contains("Java"), "error");
         }
-        Assert.assertEquals(getBaseUrl(), getCurrentUrl(), "wrong url");
-        Assert.assertEquals(getExpectedText(), searchPage.getTypedText(), "wrong text");
-        Assert.assertFalse(getBaseTitle().equals(resultPage.getPageTitle()), "page did not load");
+        Assert.assertEquals(baseUrl, currentUrl, "wrong url");
+        Assert.assertEquals(expectedText, searchPage.getTypedText(), "wrong text");
+        Assert.assertFalse(baseTitle.equals(resultPage.getPageTitle()), "page did not load");
     }
 }
